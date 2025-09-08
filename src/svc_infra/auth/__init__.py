a731e9bd15b46cdc1648f7e98c2088e9eb60c859
Ali Khatami <aliikhatami94@gmail.com>
2025-09-08 14:00:01 -0400

    bug fix

--- src/svc_infra/auth/__init__.py
+++ src/svc_infra/auth/__init__.py
@@ -1,7 +1,7 @@
 from .integration import enable_auth
-from .user_default import make_user_service
+from .user_default import make_default_user_service
 
 __all__ = [
     "enable_auth",
-    "make_user_service"
+    "make_default_user_service"
 ]
