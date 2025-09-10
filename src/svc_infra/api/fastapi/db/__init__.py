306fcf1ddba1de708ac1a8361eedb88ed70cfdbe
Ali Khatami <aliikhatami94@gmail.com>
2025-09-10 17:49:18 -0400

    added precommits

--- src/svc_infra/api/fastapi/db/__init__.py
+++ src/svc_infra/api/fastapi/db/__init__.py
@@ -1,6 +1,5 @@
+from svc_infra.api.fastapi.db.nosql import add_mongo_database, add_mongo_health, add_mongo_resources
 from svc_infra.api.fastapi.db.sql import add_sql_db, add_sql_health, add_sql_resources
-
-from .nosql import add_mongo_database, add_mongo_health, add_mongo_resources
 
 __all__ = [
     # SQL
