a0a7d13618bb094c782e848f4695fe77e51537e5
Ali Khatami <aliikhatami94@gmail.com>
2025-09-19 14:53:11 -0400

    refactoring

--- src/svc_infra/app/__init__.py
+++ src/svc_infra/app/__init__.py
@@ -1,6 +1,6 @@
 from .env import pick
 from .logging import setup_logging
-from .logging.logging import LoggingConfig, LogLevelOptions
+from .logging.formats import LoggingConfig, LogLevelOptions
 
 __all__ = [
     "setup_logging",
