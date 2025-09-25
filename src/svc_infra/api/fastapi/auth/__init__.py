357a2e10c4b5cc36f3ab4feff633f5b0ffef925f
Ali Khatami <aliikhatami94@gmail.com>
2025-09-25 00:03:39 -0400

    import issue fix

--- src/svc_infra/api/fastapi/auth/__init__.py
+++ src/svc_infra/api/fastapi/auth/__init__.py
@@ -1,9 +1,8 @@
 from .add import add_auth
-from .routing import optional_user, public_router, require_roles, require_user
+from .routing import optional_user, require_roles, require_user
 
 __all__ = [
     "add_auth",
-    "public_router",
     "optional_user",
     "require_user",
     "require_roles",
