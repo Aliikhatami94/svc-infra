833e8b8d560c68cbdeb5ef93673d3a93086467c6
Ali Khatami <aliikhatami94@gmail.com>
2025-09-30 19:48:51 -0400

    bug fix

--- src/svc_infra/api/fastapi/auth/routers/account.py
+++ src/svc_infra/api/fastapi/auth/routers/account.py
@@ -11,8 +11,8 @@ from svc_infra.api.fastapi.dual.protected import user_router
 # ---------- Router ----------
 def account_router(*, user_model: type, auth_prefix: str = "/auth") -> APIRouter:
     r = user_router(
-        prefix=f"{auth_prefix}/account",
-        tags=["auth:account"],
+        prefix=f"{auth_prefix}/users",
+        tags=["auth:users"],
     )
 
     @r.patch(
